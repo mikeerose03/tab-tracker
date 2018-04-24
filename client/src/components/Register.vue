b6fcd57c9acf61dd2b2dccca0cc218ec595ac606
Mikee Rose I. Bersabal <mribersabal@vauldex.com>
2018-04-24 15:53:31 +0800

    setup registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h1>REGISTER</h1>
+    <form @submit.prevent="register">
+      <input
+        v-model="email"
+        type="email"
+        name="email"
+        placeholder="email">
+      <input
+        v-model="password"
+        type="password"
+        name="password"
+        placeholder="password">
+      <button type="submit"> Register </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email:'123',
+      password: 'watchme'
+    }
+  },
+  methods: {
+    async register () {
+      console.log('requesting...')
+      var response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.get('/status', (req, res) => {
-  res.send({ message: 'hello world!' })
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}, You are registered!`
+  })
 })
 app.listen(process.env.PORT || 8081)
